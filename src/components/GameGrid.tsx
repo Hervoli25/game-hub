2d8c86aa4c89e98de4c5a9f13e5eb314aa2519da
Herve Tshombe <daddyhwhynot@gmail.com>
2023-04-16 18:40:13 +0200

    Build Game Cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGame from '../hooks/useGame';
+import GameCard from './GameCard';
 
 const GameGrid = () => {
   const { games, error } = useGame();
   return (
     <>
-      <ul>
-        {error && <Text>{error}</Text>}
-
+      {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
         {games.map((game) => (
-          <Text key={game.id}>{game.name}</Text>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import apiClient from "../services/api-client"
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
@@ -29,9 +30,9 @@ const useGame = () => {
         if (err.name === 'AxiosError' && err.response) {
           setError(`Error ${err.response.status}: ${err.response.statusText}`);
         } else if (err.name === 'AbortError') {
-          setError('Request was canceled');
+          setError('Request was canceled');//This is the error that is thrown when the request is aborted
         } else {
-          setError('Unknown error occurred');
+          setError('');
         }
       });
 
