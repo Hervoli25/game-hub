4fbbd5dcf82176b573b847663989ee2e7ea4d53f
Herve Tshombe <daddyhwhynot@gmail.com>
2023-04-17 00:04:12 +0200

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGame from '../hooks/useGame';
 import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
 
 const GameGrid = () => {
-  const { games, error } = useGame();
+  const { games, error, isLoading } = useGame();
+
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
-        {games.map((game) => (
-          <GameCard key={game.id} game={game} />
-        ))}
+        {isLoading
+          ? Array.from({ length: 10 }).map((_, index) => (
+              <GameCardSkeleton key={index} />
+            ))
+          : games.map((game) => <GameCard key={game.id} game={game} />)}
       </SimpleGrid>
     </>
   );
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,9 +1,8 @@
-import {CanceledError} from 'axios';
+import { CanceledError } from 'axios';
 import { useEffect, useState } from 'react';
-import apiClient from "../services/api-client"
+import apiClient from '../services/api-client';
 
-
-export interface Platform{
+export interface Platform {
   id: number;
   name: string;
   slug: string;
@@ -25,31 +24,34 @@ interface FetchGamesResponse {
 const useGame = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState<string>('');
+  const [isLoading, setLoading] = useState(false); // Set isLoading to true by default
 
   useEffect(() => {
     const controller = new AbortController();
-
+setLoading(true); // Set isLoading to true when the request is made
     apiClient
       .get<FetchGamesResponse>('/games', { signal: controller.signal })
       .then((res) => {
         console.log(res.data); // log the response data
         setGames(res.data.results);
+        setLoading(false);
       })
       .catch((err) => {
         console.error(err); // log any errors
         if (err.name === 'AxiosError' && err.response) {
           setError(`Error ${err.response.status}: ${err.response.statusText}`);
         } else if (err.name === 'AbortError') {
-          setError('Request was canceled');//This is the error that is thrown when the request is aborted
+          setError('Request was canceled'); //This is the error that is thrown when the request is aborted
         } else {
           setError('');
         }
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGame;
