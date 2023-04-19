6cb08fc5adc276ad175329cdf8e259a936a382c3
Herve Tshombe <daddyhwhynot@gmail.com>
2023-04-19 19:46:38 +0200

    Highlight the selected genre

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/logo.webp';
+import logo from '../assets/logo1.jpg';
 import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
   return (
     <HStack justifyContent='space-between' padding='10px'>
-      <Image src={logo} boxSize='60px' />
+      <Image src={logo} boxSize='60px' borderRadius={50} margin={2} />
       <ColorModeSwitch />
     </HStack>
   );
