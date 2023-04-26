4d416f6972be29c1bc0fc9ef3f16a28e6d9bf4b7
Herve Tshombe <daddyhwhynot@gmail.com>
2023-04-26 22:15:49 +0200

    Clean up GenreList

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,36 +4,38 @@ import { FaGithub, FaTwitter, FaYoutube } from 'react-icons/fa';
 const Footer = () => {
 	return (
 		<Box as='footer' py={8} bg='gray.800' color='white'>
-			<Flex direction='column' alignItems='center' justifyContent='center'>
-				<Stack direction='row' spacing={8} alignItems='center'>
-					<Link href='https://github.com'>
-						<IconButton
-							aria-label='GitHub'
-							icon={<FaGithub />}
-							colorScheme='gray'
-						/>
-					</Link>
-					<Link href='https://twitter.com'>
-						<IconButton
-							aria-label='Twitter'
-							icon={<FaTwitter />}
-							colorScheme='blue'
-						/>
-					</Link>
-					<Link href='https://youtube.com'>
-						<IconButton
-							aria-label='YouTube'
-							icon={<FaYoutube />}
-							colorScheme='red'
-						/>
-					</Link>
-				</Stack>
-				<Flex justifyContent='center' mt={4}>
-					<Text fontSize='sm'>
-						&copy; 2023 My Website. All rights reserved. By Herve Tshombe
-					</Text>
+			<Box mx='auto' textAlign='center' maxW='xl'>
+				<Flex direction='column' alignItems='center' justifyContent='center'>
+					<Stack direction='row' spacing={8} alignItems='center'>
+						<Link href='https://github.com'>
+							<IconButton
+								aria-label='GitHub'
+								icon={<FaGithub />}
+								colorScheme='gray'
+							/>
+						</Link>
+						<Link href='https://twitter.com'>
+							<IconButton
+								aria-label='Twitter'
+								icon={<FaTwitter />}
+								colorScheme='blue'
+							/>
+						</Link>
+						<Link href='https://youtube.com'>
+							<IconButton
+								aria-label='YouTube'
+								icon={<FaYoutube />}
+								colorScheme='red'
+							/>
+						</Link>
+					</Stack>
+					<Flex justifyContent='center' mt={4}>
+						<Text fontSize='sm'>
+							&copy; 2023 My Website. All rights reserved. By Herve Tshombe
+						</Text>
+					</Flex>
 				</Flex>
-			</Flex>
+			</Box>
 		</Box>
 	);
 };
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,6 +6,7 @@ import {
 	Spinner,
 	Text,
 	Button,
+	Heading,
 } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import { getCroppedImageUrl } from '../services/image-url';
@@ -21,28 +22,36 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 	if (error) return <Text>{error}</Text>;
 
 	return (
-		<List>
-			{data.map((genre) => (
-				<ListItem key={genre.id} paddingY='5px'>
-					<HStack>
-						<Image
-							boxSize='42px'
-							borderRadius={8}
-							src={getCroppedImageUrl(genre.image_background)}
-							_hover={{ cursor: 'pointer' }} // add hover styling
-						/>
-						<Button
-							fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-							onClick={() => onSelectGenre(genre)}
-							fontSize='lg'
-							variant={'link'}
-						>
-							{genre.name}
-						</Button>
-					</HStack>
-				</ListItem>
-			))}
-		</List>
+		<>
+			<Heading fontSize='2xl' marginBottom={3}>
+				Genres
+			</Heading>
+			<List>
+				{data.map((genre) => (
+					<ListItem key={genre.id} paddingY='5px'>
+						<HStack>
+							<Image
+								boxSize='42px'
+								borderRadius={8}
+								objectFit='cover'
+								src={getCroppedImageUrl(genre.image_background)}
+								_hover={{ cursor: 'pointer' }} // add hover styling
+							/>
+							<Button
+								whiteSpace={'normal'}
+								textAlign={'left'}
+								fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+								onClick={() => onSelectGenre(genre)}
+								fontSize='lg'
+								variant={'link'}
+							>
+								{genre.name}
+							</Button>
+						</HStack>
+					</ListItem>
+				))}
+			</List>
+		</>
 	);
 };
 
